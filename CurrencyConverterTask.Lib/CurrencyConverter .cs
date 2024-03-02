90453e2b488eedbba283284736368618ffdb4517
Mohammad Mahdi Talachi <mohammadmahditalachi@gmail.com>
2024-03-02 22:50:57 +0330

    feat: implement Convert

--- CurrencyConverterTask.Lib/CurrencyConverter .cs
+++ CurrencyConverterTask.Lib/CurrencyConverter .cs
@@ -41,7 +41,71 @@ public class CurrencyConverter : ICurrencyConverter
 
     public double Convert(string fromCurrency, string toCurrency, double amount)
     {
-        throw new NotImplementedException();
+        if (fromCurrency == toCurrency)
+        {
+            return amount;
+        }
+
+        var allNodes = exchangeRates.SelectMany(p => p.Value).ToList();
+
+        if (!allNodes.Any(e => e.Key == fromCurrency) || !allNodes.Any(e => e.Key == toCurrency))
+        {
+            throw new ArgumentException("Invalid currency codes.");
+        }
+
+        // Perform BFS to find the shortest conversion path
+        var queue = new Queue<string>();
+        var visited = new HashSet<string>();
+        var distTable = new Dictionary<string, (string prev, int dis)>();
+        foreach (var item in exchangeRates)
+        {
+            distTable.Add(item.Key, ("", -1));
+        }
+
+        distTable[fromCurrency] = ("", 0);
+
+        queue.Enqueue(fromCurrency);
+
+        while (queue.Count > 0)
+        {
+            var currentCurrency = queue.Dequeue();
+
+            visited.Add(currentCurrency);
+
+            foreach (var nextCurrency in exchangeRates[currentCurrency].Keys)
+            {
+                if (!visited.Contains(nextCurrency))
+                {
+                    queue.Enqueue(nextCurrency);
+                    distTable[nextCurrency] = (currentCurrency, distTable[currentCurrency].dis + 1);
+                }
+            }
+        }
+
+        if (distTable[toCurrency].dis == -1)
+            throw new ArgumentException("No conversion path found.");
+
+        List<string> path = new List<string>();
+        string node = toCurrency;
+
+        while (node != fromCurrency && distTable.ContainsKey(node))
+        {
+            path.Add(node);
+            node = distTable[node].prev;
+        }
+
+        path.Reverse();
+        double nextAmount = amount;
+        string from = fromCurrency;
+        string to = toCurrency;
+
+        foreach (var item in path)
+        {
+            nextAmount = nextAmount * exchangeRates[from][item];
+            from = item;
+        }
+
+        return nextAmount;
     }
 
     public void ClearConfiguration()
